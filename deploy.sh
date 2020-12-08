fa190077f9be8783fa9649bbd0c760837582fe3c
marcuSRB <mrussorb@gmail.com>
2020-12-08 10:33:46 +0100

    Committing the (updated) source files.

--- deploy.sh
+++ deploy.sh
@@ -57,7 +57,7 @@ cd ${WEBSITE}
 rm -rvf ${WEBSITEPUBLIC}
 
 git status
-git checkout -b feature-updated-x
+git checkout -b feature-updated-x develop
 git checkout feature-updated-x
 git add -A
 git commit -m "Committing the (updated) source files."
